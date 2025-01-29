ea865acc2b6f43aa7623e46be492fc3f14aed37e
abidk11 <abidk11@gmail.com>
2025-01-29 16:20:48 +0530

    fix auto-complete error

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -118,12 +118,6 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
 }
 
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-  } catch {
-    return {
-      message: 'Database Error: Failed to Delete Invoice.',
-    };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
